c1c06670bb0553fbe8a32576a0bb7e991cc4c4a8
Torggler <Thomas.Torggler@iCOMcept.de>
2018-03-24 15:21:58 +0100

    updates bash script

--- assets/2018/tor.sh
+++ assets/2018/tor.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+chmod +x /home/tom/ntsystemsit.github.io/assets/2018/tor.sh
 cd /home/tom/ntsystemsit.github.io
 git fetch 
 git pull
+sed -i -e 's/\r$//' ./assets/2018/tor.sh
 sed 's/https:\/\/ntsystems.it/http:\/\/ntsystcpyyew477akekwcn2od3vdknlehwof7cyt2vryieocos2sz4id.onion/' _config.yml
 sed 's/tor: false/tor: true/' _config.yml
 bundle exec jekyll build -d /var/www/html/
