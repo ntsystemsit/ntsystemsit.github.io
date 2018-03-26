311fd2f0a92f31386c8498ab4e6ef526bc7b6c00
Torggler <Thomas.Torggler@iCOMcept.de>
2018-03-26 16:03:35 +0200

    removes script

--- assets/2018/tor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-chmod +x /home/tom/ntsystemsit.github.io/assets/2018/tor.sh
-cd /home/tom/ntsystemsit.github.io
-git fetch 
-git pull
-sed -i -e 's/\r$//' ./assets/2018/tor.sh
-sed -i -e 's/https:\/\/ntsystems.it/http:\/\/ntsystcpyyew477akekwcn2od3vdknlehwof7cyt2vryieocos2sz4id.onion/' _config.yml
-sed -i -e 's/tor: false/tor: true/' _config.yml
-bundle exec jekyll build -I -d /var/www/html/
